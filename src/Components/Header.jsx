2d2b6bd8ffb2a294788c84158d839ba646e0cf6b
deedee-2904 <demaradarkwah@mail.com>
2025-06-01 19:52:34 +0100

    fixed hosted site

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,7 @@ import "./Header.css";
 export default function Header() {
 	return (
 		<header>
-			<img className="site-logo" src="./src/assets/logo.png" alt="Been There, News That! logo" />
+			<img className="site-logo" src="/logo.png" alt="Been There, News That! logo" />
 			<div className="title-and-slogan">
 				<h1>Been There, News That!</h1>
 				<p>Never be in the unknown again!</p>
